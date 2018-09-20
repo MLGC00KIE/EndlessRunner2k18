e596ee7ff5c4219557ffaa231ea7b28b198f4dae
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-21 00:16:44 +0200

    Start screen maybe.. maybe.. maybe?

---
+++ Assets/Scripts/ShakeEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShakeEffect : MonoBehaviour
+{
+    public float Magnitude = 0.5f;
+
+    private Vector3 m_startPos;
+
+    private void Start()
+    {
+        m_startPos = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        float x = Random.Range(-Magnitude, Magnitude);
+        float y = Random.Range(-Magnitude, Magnitude);
+        transform.localPosition = m_startPos + new Vector3(x, y, 0.0f);
+    }
+}
