b3aeef8a09ad8a06e64d486ca4552305167ede68
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-20 08:36:14 +0200

    Small effect test my scene (Particles)

---
+++ Assets/Scripts/GRID/GridMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GridMovement : MonoBehaviour {
+    
+    public float GridSpeed;
+    private float StartingPos;
+
+    // Use this for initialization
+    void Start () {
+        StartingPos = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position += new Vector3(0, 0, -1) * GridSpeed;
+
+		if(transform.position.z <= 3367)
+        transform.position = new Vector3(transform.position.x, transform.position.y, StartingPos);
+        
+	}
+}
