55438562672c90601bba5203b259bdfc44aab29c
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-23 14:41:46 +0200

    bit of menu control code

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public void Activate(GameObject Object)
+    {
+        Object.SetActive(true);
+    }
+
+    public void Deactivate(GameObject Object)
+    {
+        Object.SetActive(false);
+    }
+
+    public void ChangeLevel(string LevelName)
+    {
+        ChangeLevel(LevelName);
+    }
+
+    public void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
