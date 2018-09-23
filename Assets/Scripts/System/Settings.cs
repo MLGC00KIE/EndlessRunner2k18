304e9d23fb4ceffc4d11158fdee4704195b54449
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-23 16:22:04 +0200

    Bit of settings done also fps counter smh

---
+++ Assets/Scripts/System/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    public Dropdown Menu;
+    private bool fullscreen;
+
+    private void Start()
+    {
+        Menu.value = PlayerPrefs.GetInt("ResolutionValue", 0);
+    }
+
+    public void ResolutionChange(){
+        if(PlayerPrefs.GetInt("FullScreenMode", 0) == 0){
+            fullscreen = false;
+        }else{
+            fullscreen = true;
+        }
+
+        Debug.Log(Menu.value);
+
+        if (Menu.value == 0)
+        Screen.SetResolution(640, 480, fullscreen);
+        PlayerPrefs.SetInt("ResolutionValue", 0);
+
+        if (Menu.value == 1)
+        Screen.SetResolution(1280, 720, fullscreen);
+        PlayerPrefs.SetInt("ResolutionValue", 1);
+
+        if (Menu.value == 2)
+        Screen.SetResolution(1920, 1080, fullscreen);
+        PlayerPrefs.SetInt("ResolutionValue", 2);
+    }
+}
---
+++ Assets/Scripts/System/ShowFPS.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowFPS : MonoBehaviour
+{
+    float deltaTime = 0.0f;
+    public Text Text;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        Text.text = fps.ToString("F0") + "FPS";
+    }
+    
+}
