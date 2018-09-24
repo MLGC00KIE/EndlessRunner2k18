87b9c8f3bd011db2e13b7c7436524265501b7154
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-24 10:59:01 +0200

    changed the console again?

--- Assets/Scripts/System/Console.cs
+++ Assets/Scripts/System/Console.cs
@@ -16,26 +16,27 @@ public class Console : MonoBehaviour {
         string[] SplitCommand = Command.ToLower().Split(' ');
         if (SplitCommand[0] == "changelevel"){
             if (Application.CanStreamedLevelBeLoaded(SplitCommand[1])){
+                ConsoleText.text = "Changing scene to: '" + SplitCommand[1] + "'.\n" + ConsoleText.text;
                 SceneManager.LoadScene(SplitCommand[1]);
             }else{
-                ConsoleText.text = ConsoleText.text + "\nScene '" + SplitCommand[1] + "' does not exist.";
+                ConsoleText.text = "Scene '" + SplitCommand[1] + "' does not exist.\n" + ConsoleText.text;
             }
         }
         if (SplitCommand[0] == "exit"){
-            ConsoleText.text = ConsoleText.text + "\nThis will close the console someday in the future! :)";
+            ConsoleText.text = "This will close the console someday in the future! :)\n" + ConsoleText.text;
         }
         if (SplitCommand[0] == "quit"){
             Application.Quit();
         }
         if (SplitCommand[0] == "help"){
-            ConsoleText.text = ConsoleText.text + "\nThese commands are available: \n - ChangeLevel + level name (Changes the level),\n - Exit (Exits console) ,\n - Quit (Quits the application),\n - Help (Shows this screen),\n";
+            ConsoleText.text = "These commands are available: \n - ChangeLevel + [level name] (Changes the level)\n - Exit (Exits console) \n - Quit (Quits the application)\n - Help (Shows this screen)\n" + ConsoleText.text;
         }
         if (SplitCommand[0] == ""){
-            ConsoleText.text = ConsoleText.text + "\nThis cannot be left empty.";
+            ConsoleText.text =  "This cannot be left empty.\n" + ConsoleText.text;
         }
         else
         {
-            ConsoleText.text = ConsoleText.text + "\nCommand '" + SplitCommand[0] + "' not found.";
+            ConsoleText.text = "Command '" + SplitCommand[0] + "' not found.\n" + ConsoleText.text;
         }
     }
 
