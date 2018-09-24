44cadb787615c7dfa5e796eee6e5a80e1805e9a6
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-24 22:11:47 +0200

    No more double spawning of dont destroy on load

---
+++ Assets/Scripts/System/DontDestroyOnLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+    private static DontDestroyOnLoad instance = null;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            return;
+        }
+        Destroy(this.gameObject);
+    }
+}
