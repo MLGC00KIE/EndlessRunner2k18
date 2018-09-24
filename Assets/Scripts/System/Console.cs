5d60f86389341f0a137d5f41c56d84370a20e9e6
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-24 13:02:24 +0200

    oh

--- Assets/Scripts/System/Console.cs
+++ Assets/Scripts/System/Console.cs
@@ -8,9 +8,8 @@ public class Console : MonoBehaviour {
     public InputField InputField;
     string Command;
     public Text ConsoleText;
-    
-    public void NewCommand()
-    {
+
+    public void NewCommand(){
         Command = InputField.text;
         InputField.text = "";
         string[] SplitCommand = Command.ToLower().Split(' ');
@@ -18,25 +17,43 @@ public class Console : MonoBehaviour {
             if (Application.CanStreamedLevelBeLoaded(SplitCommand[1])){
                 ConsoleText.text = "Changing scene to: '" + SplitCommand[1] + "'.\n" + ConsoleText.text;
                 SceneManager.LoadScene(SplitCommand[1]);
+            }
+            else if (SplitCommand[1] == ""){
+                ConsoleText.text = "<color=red><b>Please, Specify a stage.</b>\n</color>" + ConsoleText.text;
             }else{
-                ConsoleText.text = "Scene '" + SplitCommand[1] + "' does not exist.\n" + ConsoleText.text;
+                ConsoleText.text = "Scene <color=red>'" + SplitCommand[1] + "'</color> does <b>not</b> exist.\n" + ConsoleText.text;
             }
         }
-        if (SplitCommand[0] == "exit"){
+        else if (SplitCommand[0] == "exit"){
             ConsoleText.text = "This will close the console someday in the future! :)\n" + ConsoleText.text;
         }
-        if (SplitCommand[0] == "quit"){
+        else if (SplitCommand[0] == "quit"){
+            ConsoleText.text = "<b>Quitting application..\n</b>" + ConsoleText.text;
             Application.Quit();
+            ConsoleText.text = "<color=red>Warning: FAILED TO QUIT.</color>\n" + ConsoleText.text;
         }
-        if (SplitCommand[0] == "help"){
-            ConsoleText.text = "These commands are available: \n - ChangeLevel + [level name] (Changes the level)\n - Exit (Exits console) \n - Quit (Quits the application)\n - Help (Shows this screen)\n" + ConsoleText.text;
+        else if (SplitCommand[0] == "clear"){
+            ConsoleText.text = "";
         }
-        if (SplitCommand[0] == ""){
-            ConsoleText.text =  "This cannot be left empty.\n" + ConsoleText.text;
+        else if (SplitCommand[0] == "timescale")
+        {
+            if (float.Parse(SplitCommand[1]) < 0){
+                ConsoleText.text = "<color=red>TimeScale cannot be lower than 0.</color>\n" + ConsoleText.text;
+            }
+            else{
+                Time.timeScale = float.Parse(SplitCommand[1]);
+                ConsoleText.text = "TimeScale set to: " + SplitCommand[1] + ".\n" + ConsoleText.text;
+            }
+        }
+        else if (SplitCommand[0] == ""){
+            ConsoleText.text = "This cannot be left empty.\n" + ConsoleText.text;
+        }
+        else if (SplitCommand[0] == "help"){
+            ConsoleText.text = "________________________________\nThese commands are available: \n - ChangeLevel + [level name]\n - Exit\n - Quit\n - Help\n - Clear\n - TimeScale [ammount]\n" + ConsoleText.text;
         }
         else
         {
-            ConsoleText.text = "Command '" + SplitCommand[0] + "' not found.\n" + ConsoleText.text;
+            ConsoleText.text = "Unknown command.\nTry using 'Help'.\n" + ConsoleText.text;
         }
     }
 
