5a6fd5aebed2c29acdaf3141ce54555a6acb6b5d
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-24 10:26:58 +0200

    moar console maybe?

--- Assets/Scripts/System/Console.cs
+++ Assets/Scripts/System/Console.cs
@@ -7,20 +7,35 @@ using UnityEngine.UI;
 public class Console : MonoBehaviour {
     public InputField InputField;
     string Command;
+    public Text ConsoleText;
     
     public void NewCommand()
     {
         Command = InputField.text;
+        InputField.text = "";
         string[] SplitCommand = Command.ToLower().Split(' ');
-
         if (SplitCommand[0] == "changelevel"){
             if (Application.CanStreamedLevelBeLoaded(SplitCommand[1])){
                 SceneManager.LoadScene(SplitCommand[1]);
             }else{
-                SceneManager.LoadScene("error");
+                ConsoleText.text = ConsoleText.text + "\nScene '" + SplitCommand[1] + "' does not exist.";
             }
-        }else{
-            Debug.Log("Command not found");
+        }
+        if (SplitCommand[0] == "exit"){
+            ConsoleText.text = ConsoleText.text + "\nThis will close the console someday in the future! :)";
+        }
+        if (SplitCommand[0] == "quit"){
+            Application.Quit();
+        }
+        if (SplitCommand[0] == "help"){
+            ConsoleText.text = ConsoleText.text + "\nThese commands are available: \n - ChangeLevel + level name (Changes the level),\n - Exit (Exits console) ,\n - Quit (Quits the application),\n - Help (Shows this screen),\n";
+        }
+        if (SplitCommand[0] == ""){
+            ConsoleText.text = ConsoleText.text + "\nThis cannot be left empty.";
+        }
+        else
+        {
+            ConsoleText.text = ConsoleText.text + "\nCommand '" + SplitCommand[0] + "' not found.";
         }
     }
 
