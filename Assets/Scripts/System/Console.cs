9156204c29b462fad0f1565e0c710b0f9ea4e03e
Sjors Kaagman <sjorspnk@gmail.com>
2018-09-24 09:50:04 +0200

    command menu thing lol beta 2.0.01

---
+++ Assets/Scripts/System/Console.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Console : MonoBehaviour {
+    public InputField InputField;
+    string Command;
+    
+    public void NewCommand()
+    {
+        Command = InputField.text;
+        string[] SplitCommand = Command.ToLower().Split(' ');
+
+        if (SplitCommand[0] == "changelevel"){
+            if (Application.CanStreamedLevelBeLoaded(SplitCommand[1])){
+                SceneManager.LoadScene(SplitCommand[1]);
+            }else{
+                SceneManager.LoadScene("error");
+            }
+        }else{
+            Debug.Log("Command not found");
+        }
+    }
+
+
+}
