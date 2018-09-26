07e6b5078f771087b801db1e58574c3e25d2ded8
MLGC00KIE <24893@ma-web.nl>
2018-09-26 19:49:43 +0200

    added growing animations and some more hsit

---
+++ Assets/Scripts/Objects/GrowAfterSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowAfterSpawn : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 newScale;
+    [SerializeField]
+    private float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.gameObject.transform.localScale = Vector3.Lerp(this.gameObject.transform.localScale, newScale, Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+
+        this.gameObject.transform.localScale = Vector3.Lerp(this.gameObject.transform.localScale, newScale, Time.deltaTime * speed);
+
+    }
+}
