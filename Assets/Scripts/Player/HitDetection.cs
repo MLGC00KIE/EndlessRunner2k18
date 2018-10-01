0091a68f91ed33b2ed074289a99a0b6665227b7f
MLGC00KIE <24893@ma-web.nl>
2018-10-01 14:27:51 +0200

    sortof working player lives...

--- Assets/Scripts/Player/HitDetection.cs
+++ Assets/Scripts/Player/HitDetection.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HitDetection : MonoBehaviour {
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    [SerializeField]
+    private float timeBetweenHit;
+
+    private bool canHit;
+    Lives l;
+
+    private float elapsed;
+
+    private void Awake()
+    {
+        l = GetComponent<Lives>();
+    }
+    // Update is called once per frame
+    void Update () {
+        elapsed += Time.deltaTime;
+        if (elapsed >= timeBetweenHit)
+        {
+            canHit = true;
+        } else
+        {
+            canHit = false;
+        }
+    }
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.transform.tag == "wall")
         {
-            Debug.Log("got hit by " + col.transform.name);
-            // remove health scripty thingy
+            if (canHit)
+            {
+                Debug.Log("got hit by " + col.transform.name);
+                l.SetHealth(l.GetHealth() - 1);
+                elapsed = elapsed % timeBetweenHit;
+                canHit = false;
+            } else if (!canHit)
+            {
+                Debug.Log("nah fam");
+            }
         }
     }
+
+
 }
---
+++ Assets/Scripts/Player/Lives.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject secondPart;
+
+    [SerializeField]
+    private int lives = 2;
+	
+	// Update is called once per frame
+	void Update () {
+		if (lives <= 1)
+        {
+            secondPart.SetActive(false);
+        }
+        if (lives > 1) {
+            secondPart.SetActive(true);
+        }
+	}
+
+    public void SetHealth(int newHealth)
+    {
+        lives = newHealth;
+    }
+
+    public int GetHealth()
+    {
+        return lives;
+    }
+
+}
