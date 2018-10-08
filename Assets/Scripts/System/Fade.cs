a33cebe90f180075ba999862a291485eb36b74d9
Sjors Kaagman <sjorspnk@gmail.com>
2018-10-08 15:01:52 +0200

    fade for scenes test lol.

---
+++ Assets/Scripts/System/Fade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+
+	void Update () {
+        Color tmp = gameObject.GetComponent<Image>().color;
+        tmp.a += 0.01f;
+        gameObject.GetComponent<Image>().color = tmp;
+        if(tmp.a >= 1)
+        {
+            SceneManager.LoadScene("error");
+        }
+    }
+}
