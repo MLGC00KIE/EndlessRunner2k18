42a71bca9606156ae4dbe9cce33a3674110917a1
Sjors Kaagman <sjorspnk@gmail.com>
2018-10-11 13:20:36 +0200

    ik heb iets gedaan?

---
+++ Assets/Scripts/CountDownFix.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CountDownFix : MonoBehaviour {
+    bool Loaded;
+
+	void Update () {
+        Debug.Log(SceneManager.GetActiveScene().name);
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            if (!Loaded)
+            {
+                SceneManager.LoadScene("Game");
+                Loaded = true;
+            }
+        }
+        else
+        {
+            Loaded = false;
+        }
+
+	}
+}
